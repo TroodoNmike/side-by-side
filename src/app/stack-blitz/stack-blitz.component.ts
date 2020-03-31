b65b60bd8dd293ac8a18e379a9cd2682263f9f83
Michal Klik <michal.klik@gmail.com>
2020-03-31 20:32:41 +0200

    add stackblitz test export

--- src/app/stack-blitz/stack-blitz.component.ts
+++ src/app/stack-blitz/stack-blitz.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import sdk from '@stackblitz/sdk';
+
+import { SelectedComponentSingleRouteMapping } from '../page/page-content.inteface';
 
 @Component({
     selector: 'app-stack-blitz',
@@ -7,11 +10,48 @@ import { Component } from '@angular/core';
 })
 export class StackBlitzComponent {
 
+    @Input() selectedComponentRouteMapping: SelectedComponentSingleRouteMapping = new SelectedComponentSingleRouteMapping();
+
     constructor() {
     }
 
     submit(): void {
         alert('wip');
+        const code = `import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+enableProdMode();
+platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+
+`;
+        const html = `<h1>I was created on <span id='time'></span></h1>`;
+
+        const files = {
+            'main.ts': code,
+            'index.html': html
+        };
+
+        this.selectedComponentRouteMapping.componentData.forEach(data => {
+            data.tabsData.forEach(item => {
+                files['src/' + item.title] = item.source;
+            });
+        });
+
+        const project = this.createProject(files);
+        sdk.openProject(project);
     }
 
+    private createProject(files) {
+
+        return {
+            files,
+            title: 'Dynamically Generated Project',
+            description: 'Created with <3 by the StackBlitz SDK!',
+            template: 'angular-cli',
+            dependencies: {
+            }
+        };
+    }
 }
